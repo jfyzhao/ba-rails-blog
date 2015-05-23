4388c61c746adf56c317ec0897dfd25c59fb9cf6
Jeffrey Zhao <jeffreyzhao@college.harvard.edu>
2015-05-23 18:36:57 -0500

    changed articles to posts in routes, added rspec to gemfile

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :articles do
+  resources :posts, controller: 'articles', as: 'articles' do
     resources :comments
   end
 
